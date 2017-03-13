5e03db0d3e8ddc5183d379cc3dcd317dd653a3aa
Katrina <katrina.huber@gmail.com>
2017-03-12 18:47:46 -0700

    fully coded

--- flask/server.py
+++ flask/server.py
@@ -13,6 +13,19 @@ app.secret_key = "ABC"
 def home():
     return render_template('index.html')
 
+@app.route('/application-form')
+def form():
+    jobs = ["Mindless Drone", "Software Engineer", "QA Engineer", "Product Manager", "Code Monkey"]
+    return render_template('application-form.html', jobs=jobs)
+
+@app.route('/application-response', methods=['POST'])
+def response():
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    salary = float(request.form.get('salary'))
+    job = request.form.get('job')
+    return render_template('application-response.html', firstname=firstname, lastname=lastname, salary="${:,.2f}".format(salary), job=job)
+
 
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the
